b1a4960deaa1a9393d08573768b4965173d8f913
Marcus Urani <marcus.urani20@gmail.com>
2022-01-08 11:23:24 -0300

    Finalizando a estilização e mudando a forma que o visor é limpo

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-const displayNum = (number) => {
-  let resultArea = document.getElementById("text1");
-  resultArea.value = resultArea.value + number;
+const visor = document.getElementById("text1");
+const cButton = document.getElementById("cBtn");
+
+const showResult = (number) => {
+  visor.value = visor.value + number;
 };
+
+cButton.addEventListener("click", () => {
+  visor.value = "";
+});
